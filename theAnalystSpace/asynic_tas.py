a2eee999770f79f1b927aaa09669101a9db1aabd
Greenbirch2007 <291109028@qq.com>
2020-10-11 20:09:02 +0800

    用协程改进

--- theAnalystSpace/asynic_tas.py
+++ theAnalystSpace/asynic_tas.py
@@ -1,5 +1,13 @@
-#! -*- coding:utf-8 -*-
 
+import datetime
+
+import pymysql
+import requests
+from lxml import etree
+import json
+from queue import Queue
+import threading
+from requests.exceptions import RequestException
 
 import datetime
 import re
@@ -93,25 +101,61 @@ def parse_stock_note(html):
 
 
 
+import asyncio
+import aiohttp
+
+
+def run_forever(func):
+    def wrapper(obj):
+        while True:
+            func(obj)
+    return wrapper
+
+def find_longest_str(str_list):
+    '''
+    找到列表中字符串最长的位置索引
+    先获取列表中每个字符串的长度，查找长度最大位置的索引值即可
+    '''
+    num_list = [len(one) for one in str_list]
+    index_num = num_list.index(max(num_list))
+    return str_list[int(index_num)]
+
+
+
+
+
+async def get_title(url):
+
+
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+
+            text = await resp.text()
+            patt = re.compile('<td height="30" colspan="2" align="left" class="wz">附件名称："(.*?)"</td>', re.S)
+            items = re.findall(patt, text)
+            title = items[0]
+            f_url = url + '&down=1'
+            file_name = wget.download(f_url, out=title)
+            print(file_name)
+
+
+
+
+
+
 if __name__ == '__main__':
+    big_list=[]
 
 
     # 默认访问本目录
     excelFile = 'tas_link.xlsx'
     full_items = read_xlrd(excelFile=excelFile)
     for i in full_items:
-        url1= i[0]
-        f_url =i[0]+'&down=1'
-        html = call_page(url1)
-        patt = re.compile('<td height="30" colspan="2" align="left" class="wz">附件名称："(.*?)"</td>', re.S)
-        items = re.findall(patt, html)
-        title=items[0]
-        file_name = wget.download(f_url, out=title)
-        print(file_name)
+        url1 = i[0]
+        big_list.append(url1)
+    print(big_list)
 
-        # 直接在这里下载
-
-
-
-
-
+    loop = asyncio.get_event_loop()
+    fun_list = (get_title(i) for i in big_list)
+    loop.run_until_complete(asyncio.gather(*fun_list))
